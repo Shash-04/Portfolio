e4417ed4a1786ffefe24fd3e52bdb7c4fc543261
Shash-04 <shashwatvaish2@gmail.com>
2025-05-19 16:37:49 +0530

    idk old

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -11,7 +11,7 @@ import dynamic from 'next/dynamic';
 // Dynamically import Lottie with SSR disabled
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
-import devAnimation from "@/public/Developer.json";
+import devAnimation from "@/public/Developer.json"
 
 export function HeroSection() {
   return (
